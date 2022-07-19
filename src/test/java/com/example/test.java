66194faee5a94a9d7155afeb0608ca95efeafc82
Anirudh0030 <100831633+Anirudh0030@users.noreply.github.com>
2022-07-19 23:20:13 +0530

    New commit

---
+++ src/test/java/com/example/test.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class test {
+
+  /**
+ * @param args
+ * @throws InterruptedException
+ */
+public static void main (String [] args) throws InterruptedException {
+     
+    WebDriverManager.chromedriver().setup();
+    WebDriver driver = new ChromeDriver();
+    driver.get("https://www.google.com");
+    Thread.sleep (1000);
+    driver.close(); 
+
+
+    }
+    
+}
